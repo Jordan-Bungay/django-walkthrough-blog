667f5c435af2fa4765d2e3b71c4c4f1534512ca9
Jordan Bungay <jordan.bungay@hotmail.com>
2024-09-20 09:26:08 +0000

    Add meta data to the comment section

--- blog/models.py
+++ blog/models.py
@@ -33,3 +33,11 @@ class Comment(models.Model):
     body = models.TextField()
     approved = models.BooleanField(default=False)
     created_on = models.DateTimeField(auto_now_add=True)
+
+    # Metadata
+    class Meta:
+        ordering = ["created_on"]
+    
+    # Method
+    def __str__(self):
+        return f"Comment {self.body} by {self.author}"
